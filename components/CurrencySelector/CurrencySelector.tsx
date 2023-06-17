d295aec97a517e458f79dd23586c0288727b2d33
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2023-06-17 20:12:55 +0200

    deat: add featching errors

--- components/CurrencySelector/CurrencySelector.tsx
+++ components/CurrencySelector/CurrencySelector.tsx
@@ -1,6 +1,6 @@
+'use client'
 import { ChangeEvent } from 'react';
 import styles from './CurrencySelector.module.scss';
-
 
 export default function CurrencySelector({
   value,
--- components/CurrencySelectorOptions.tsx
+++ components/CurrencySelectorOptions.tsx
@@ -2,11 +2,16 @@ import { getCurrenciesList } from "@/api/exchangerateApi";
 
 export default async function CurrencySelectorOptions() {
   const currancies = await getCurrenciesList();
-  return (
+
+  if (currancies) {
+    return (
       <>
         {currancies.map((currency: string[]) => (
           <option key={currency[0]} value={currency[0]} className="currancies-select__options">{`${currency[0]} ${currency[1]}`}</option>
         ))}
       </>
   );
+  } else { 
+    return <option value="USD">USD Dollar</option>
+  }
 };
