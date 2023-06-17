7108b92070682cdfa48dcb1457e1a22eedc2d4e7
Yaraslau Borodavkin <y.borodavkin2011@gmail.com>
2023-06-17 20:38:38 +0200

    feat; change comonent

--- components/CurrencySelectorOptions.tsx
+++ components/CurrencySelectorOptions.tsx
@@ -1,17 +1,17 @@
 import { getCurrenciesList } from "@/api/exchangerateApi";
 
 export default async function CurrencySelectorOptions() {
-  const currancies = await getCurrenciesList();
+  // const currancies = await getCurrenciesList();
 
-  if (currancies) {
-    return (
-      <>
-        {currancies.map((currency: string[]) => (
-          <option key={currency[0]} value={currency[0]} className="currancies-select__options">{`${currency[0]} ${currency[1]}`}</option>
-        ))}
-      </>
-  );
-  } else { 
+  // if (currancies) {
+  //   return (
+  //     <>
+  //       {currancies.map((currency: string[]) => (
+  //         <option key={currency[0]} value={currency[0]} className="currancies-select__options">{`${currency[0]} ${currency[1]}`}</option>
+  //       ))}
+  //     </>
+  // );
+  // } else { 
     return <option value="USD">USD Dollar</option>
-  }
+  // }
 };
